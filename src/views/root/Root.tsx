e21466bd6106f09dc2c1d71b2cfc280c278d3fd2
PatrykAdamski <patada7090@gmail.com>
2022-04-26 20:41:10 +0200

    Header rwd view and implement button component

--- src/views/root/Root.tsx
+++ src/views/root/Root.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Navigation from "../../components/navigation/Navigation";
+import { Navigation } from "../../components/navigation/Navigation";
+import { Header } from "../../components/header/Header";
 
 function Root() {
   return (
     <>
       <Navigation />
+      <Header />
     </>
   );
 }
